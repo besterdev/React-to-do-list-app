fb975fd6759b472151b4b668cc71a6a30de08ee4
Bester Dev <thawatchai.krai@gmail.com>
2020-06-15 21:59:44 +0700

    Updates

---
+++ precache-manifest.f726a51c0ae238e21bcf678548e125bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b35bddd6175b946892dc64d13d5f7473",
+    "url": "/React-to-do-list-app/index.html"
+  },
+  {
+    "revision": "7d14c9c8ef4bcd3aacc9",
+    "url": "/React-to-do-list-app/static/css/main.60e6b25a.chunk.css"
+  },
+  {
+    "revision": "7c7bae515710c9819edf",
+    "url": "/React-to-do-list-app/static/js/2.62457498.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-to-do-list-app/static/js/2.62457498.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d14c9c8ef4bcd3aacc9",
+    "url": "/React-to-do-list-app/static/js/main.6333a2cf.chunk.js"
+  },
+  {
+    "revision": "ca9c290845028c2beccf",
+    "url": "/React-to-do-list-app/static/js/runtime-main.0831bdb3.js"
+  }
+]);
